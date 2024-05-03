49bbe90d02a2ca1994314a27b7e824bb3a6785cd
cfreal <folcharles@gmail.com>
2024-05-03 16:20:48 +0200

    Fixed str->bytes conversion for most of the code

--- wrapwrap.py
+++ wrapwrap.py
@@ -138,7 +138,9 @@ class WrapWrap:
         self.filters.append(filters)
         return self
 
-    def push_char(self, c: str) -> None:
+    def push_char(self, c: bytes) -> None:
+        if isinstance(c, int):
+            c = bytes((c,))
         return self / self.conversions[c] / B64D / B64E
 
     def push_char_safely(self, c: str) -> None:
@@ -166,12 +168,12 @@ class WrapWrap:
         The second B64 needs to be 3-aligned because the third needs to be 4-aligned.
         """
         self / B64E / QPE / REMOVE_EQUAL
-        self.push_char("A")
+        self.push_char(b"A")
         self / QPE / REMOVE_EQUAL
-        self.push_char("A")
+        self.push_char(b"A")
         self / QPE / REMOVE_EQUAL
-        self.push_char_safely("A")
-        self.push_char_safely("A")
+        self.push_char_safely(b"A")
+        self.push_char_safely(b"A")
         self / B64D
 
     def escape(self) -> None:
@@ -187,7 +189,7 @@ class WrapWrap:
         self.align()
         self / "convert.iconv.437.UCS-4le"
 
-    def add3_swap(self, triplet: str):
+    def add3_swap(self, triplet: bytes):
         assert len(triplet) == 3, f"add3 called with: {triplet}"
         b64 = self.b64e(triplet)
         self / B64E
@@ -198,14 +200,14 @@ class WrapWrap:
         self / B64D
         self / SWAP4
 
-    def b64e(self, value: str, strip: bool = False) -> str:
+    def b64e(self, value: str, strip: bool = False) -> bytes:
         value = tf.base64.encode(value)
         if strip:
             while value.endswith("="):
                 value = value.removesuffix("=")
-        return value
+        return value.encode()
 
-    def add_suffix(self):
+    def add_suffix(self) -> None:
         """Adds a suffix to the string, along with the <LF>0<LF> that marks the end of
         chunked data.
         """
@@ -241,7 +243,7 @@ class WrapWrap:
             + 7
             + len(prefix)
         )
-        chunk_header = self.align_left(f"{size:x}\n", 3, "0")
+        chunk_header = self.align_left(f"{size:x}\n".encode(), 3, b"0")
         b64 = self.b64e(chunk_header + prefix)
         for char in reversed(b64):
             self.push_char_safely(char)
@@ -255,11 +257,11 @@ class WrapWrap:
         therefore "remove" that char.
         """
         char = chunk[2]
-        alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/"
+        alphabet = b"ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/"
         index = alphabet.find(char)
-        return chunk[:2] + alphabet[index + 3]
+        return chunk[:2] + alphabet[index + 3: index + 3 + 1]
 
-    def align_right(self, input_str: str, n: int, p: str = None) -> str:
+    def align_right(self, input_str: str, n: int, p: str = None) -> bytes:
         """Aligns the input string to the right to make its length divisible by n, using
         the specified pad character.
         """
@@ -270,7 +272,7 @@ class WrapWrap:
 
         return aligned_str
 
-    def align_left(self, input_str: str, n: int, p: str = None) -> str:
+    def align_left(self, input_str: str, n: int, p: str = None) -> bytes:
         """Aligns the input string to the left to make its length divisible by n, using
         the specified pad character.
         """
@@ -284,70 +286,70 @@ class WrapWrap:
         return value + (div - value % div) % div
 
     conversions = {
-        "0": "convert.iconv.UTF8.UTF16LE|convert.iconv.UTF8.CSISO2022KR|convert.iconv.UCS2.UTF8|convert.iconv.8859_3.UCS2",
-        "1": "convert.iconv.ISO88597.UTF16|convert.iconv.RK1048.UCS-4LE|convert.iconv.UTF32.CP1167|convert.iconv.CP9066.CSUCS4",
-        "2": "convert.iconv.L5.UTF-32|convert.iconv.ISO88594.GB13000|convert.iconv.CP949.UTF32BE|convert.iconv.ISO_69372.CSIBM921",
-        "3": "convert.iconv.L6.UNICODE|convert.iconv.CP1282.ISO-IR-90|convert.iconv.ISO6937.8859_4|convert.iconv.IBM868.UTF-16LE",
-        "4": "convert.iconv.CP866.CSUNICODE|convert.iconv.CSISOLATIN5.ISO_6937-2|convert.iconv.CP950.UTF-16BE",
-        "5": "convert.iconv.UTF8.UTF16LE|convert.iconv.UTF8.CSISO2022KR|convert.iconv.UTF16.EUCTW|convert.iconv.8859_3.UCS2",
-        "6": "convert.iconv.INIS.UTF16|convert.iconv.CSIBM1133.IBM943|convert.iconv.CSIBM943.UCS4|convert.iconv.IBM866.UCS-2",
-        "7": "convert.iconv.851.UTF-16|convert.iconv.L1.T.618BIT|convert.iconv.ISO-IR-103.850|convert.iconv.PT154.UCS4",
-        "8": "convert.iconv.ISO2022KR.UTF16|convert.iconv.L6.UCS2",
-        "9": "convert.iconv.CSIBM1161.UNICODE|convert.iconv.ISO-IR-156.JOHAB",
-        "A": "convert.iconv.8859_3.UTF16|convert.iconv.863.SHIFT_JISX0213",
-        "a": "convert.iconv.CP1046.UTF32|convert.iconv.L6.UCS-2|convert.iconv.UTF-16LE.T.61-8BIT|convert.iconv.865.UCS-4LE",
-        "B": "convert.iconv.CP861.UTF-16|convert.iconv.L4.GB13000",
-        "b": "convert.iconv.JS.UNICODE|convert.iconv.L4.UCS2|convert.iconv.UCS-2.OSF00030010|convert.iconv.CSIBM1008.UTF32BE",
-        "C": "convert.iconv.UTF8.CSISO2022KR",
-        "c": "convert.iconv.L4.UTF32|convert.iconv.CP1250.UCS-2",
-        "D": "convert.iconv.INIS.UTF16|convert.iconv.CSIBM1133.IBM943|convert.iconv.IBM932.SHIFT_JISX0213",
-        "d": "convert.iconv.INIS.UTF16|convert.iconv.CSIBM1133.IBM943|convert.iconv.GBK.BIG5",
-        "E": "convert.iconv.IBM860.UTF16|convert.iconv.ISO-IR-143.ISO2022CNEXT",
-        "e": "convert.iconv.JS.UNICODE|convert.iconv.L4.UCS2|convert.iconv.UTF16.EUC-JP-MS|convert.iconv.ISO-8859-1.ISO_6937",
-        "F": "convert.iconv.L5.UTF-32|convert.iconv.ISO88594.GB13000|convert.iconv.CP950.SHIFT_JISX0213|convert.iconv.UHC.JOHAB",
-        "f": "convert.iconv.CP367.UTF-16|convert.iconv.CSIBM901.SHIFT_JISX0213",
-        "g": "convert.iconv.SE2.UTF-16|convert.iconv.CSIBM921.NAPLPS|convert.iconv.855.CP936|convert.iconv.IBM-932.UTF-8",
-        "G": "convert.iconv.L6.UNICODE|convert.iconv.CP1282.ISO-IR-90",
-        "H": "convert.iconv.CP1046.UTF16|convert.iconv.ISO6937.SHIFT_JISX0213",
-        "h": "convert.iconv.CSGB2312.UTF-32|convert.iconv.IBM-1161.IBM932|convert.iconv.GB13000.UTF16BE|convert.iconv.864.UTF-32LE",
-        "I": "convert.iconv.L5.UTF-32|convert.iconv.ISO88594.GB13000|convert.iconv.BIG5.SHIFT_JISX0213",
-        "i": "convert.iconv.DEC.UTF-16|convert.iconv.ISO8859-9.ISO_6937-2|convert.iconv.UTF16.GB13000",
-        "J": "convert.iconv.863.UNICODE|convert.iconv.ISIRI3342.UCS4",
-        "j": "convert.iconv.CP861.UTF-16|convert.iconv.L4.GB13000|convert.iconv.BIG5.JOHAB|convert.iconv.CP950.UTF16",
-        "K": "convert.iconv.863.UTF-16|convert.iconv.ISO6937.UTF16LE",
-        "k": "convert.iconv.JS.UNICODE|convert.iconv.L4.UCS2",
-        "L": "convert.iconv.IBM869.UTF16|convert.iconv.L3.CSISO90|convert.iconv.R9.ISO6937|convert.iconv.OSF00010100.UHC",
-        "l": "convert.iconv.CP-AR.UTF16|convert.iconv.8859_4.BIG5HKSCS|convert.iconv.MSCP1361.UTF-32LE|convert.iconv.IBM932.UCS-2BE",
-        "M": "convert.iconv.CP869.UTF-32|convert.iconv.MACUK.UCS4|convert.iconv.UTF16BE.866|convert.iconv.MACUKRAINIAN.WCHAR_T",
-        "m": "convert.iconv.SE2.UTF-16|convert.iconv.CSIBM921.NAPLPS|convert.iconv.CP1163.CSA_T500|convert.iconv.UCS-2.MSCP949",
-        "N": "convert.iconv.CP869.UTF-32|convert.iconv.MACUK.UCS4",
-        "n": "convert.iconv.ISO88594.UTF16|convert.iconv.IBM5347.UCS4|convert.iconv.UTF32BE.MS936|convert.iconv.OSF00010004.T.61",
-        "O": "convert.iconv.CSA_T500.UTF-32|convert.iconv.CP857.ISO-2022-JP-3|convert.iconv.ISO2022JP2.CP775",
-        "o": "convert.iconv.JS.UNICODE|convert.iconv.L4.UCS2|convert.iconv.UCS-4LE.OSF05010001|convert.iconv.IBM912.UTF-16LE",
-        "P": "convert.iconv.SE2.UTF-16|convert.iconv.CSIBM1161.IBM-932|convert.iconv.MS932.MS936|convert.iconv.BIG5.JOHAB",
-        "p": "convert.iconv.IBM891.CSUNICODE|convert.iconv.ISO8859-14.ISO6937|convert.iconv.BIG-FIVE.UCS-4",
-        "q": "convert.iconv.SE2.UTF-16|convert.iconv.CSIBM1161.IBM-932|convert.iconv.GBK.CP932|convert.iconv.BIG5.UCS2",
-        "Q": "convert.iconv.L6.UNICODE|convert.iconv.CP1282.ISO-IR-90|convert.iconv.CSA_T500-1983.UCS-2BE|convert.iconv.MIK.UCS2",
-        "R": "convert.iconv.PT.UTF32|convert.iconv.KOI8-U.IBM-932|convert.iconv.SJIS.EUCJP-WIN|convert.iconv.L10.UCS4",
-        "r": "convert.iconv.IBM869.UTF16|convert.iconv.L3.CSISO90|convert.iconv.ISO-IR-99.UCS-2BE|convert.iconv.L4.OSF00010101",
-        "S": "convert.iconv.INIS.UTF16|convert.iconv.CSIBM1133.IBM943|convert.iconv.GBK.SJIS",
-        "s": "convert.iconv.IBM869.UTF16|convert.iconv.L3.CSISO90",
-        "T": "convert.iconv.L6.UNICODE|convert.iconv.CP1282.ISO-IR-90|convert.iconv.CSA_T500.L4|convert.iconv.ISO_8859-2.ISO-IR-103",
-        "t": "convert.iconv.864.UTF32|convert.iconv.IBM912.NAPLPS",
-        "U": "convert.iconv.INIS.UTF16|convert.iconv.CSIBM1133.IBM943",
-        "u": "convert.iconv.CP1162.UTF32|convert.iconv.L4.T.61",
-        "V": "convert.iconv.CP861.UTF-16|convert.iconv.L4.GB13000|convert.iconv.BIG5.JOHAB",
-        "v": "convert.iconv.UTF8.UTF16LE|convert.iconv.UTF8.CSISO2022KR|convert.iconv.UTF16.EUCTW|convert.iconv.ISO-8859-14.UCS2",
-        "W": "convert.iconv.SE2.UTF-16|convert.iconv.CSIBM1161.IBM-932|convert.iconv.MS932.MS936",
-        "w": "convert.iconv.MAC.UTF16|convert.iconv.L8.UTF16BE",
-        "X": "convert.iconv.PT.UTF32|convert.iconv.KOI8-U.IBM-932",
-        "x": "convert.iconv.CP-AR.UTF16|convert.iconv.8859_4.BIG5HKSCS",
-        "Y": "convert.iconv.CP367.UTF-16|convert.iconv.CSIBM901.SHIFT_JISX0213|convert.iconv.UHC.CP1361",
-        "y": "convert.iconv.851.UTF-16|convert.iconv.L1.T.618BIT",
-        "Z": "convert.iconv.SE2.UTF-16|convert.iconv.CSIBM1161.IBM-932|convert.iconv.BIG5HKSCS.UTF16",
-        "z": "convert.iconv.865.UTF16|convert.iconv.CP901.ISO6937",
-        "/": "convert.iconv.IBM869.UTF16|convert.iconv.L3.CSISO90|convert.iconv.UCS2.UTF-8|convert.iconv.CSISOLATIN6.UCS-4",
-        "+": "convert.iconv.UTF8.UTF16|convert.iconv.WINDOWS-1258.UTF32LE|convert.iconv.ISIRI3342.ISO-IR-157",
+        b"0": "convert.iconv.UTF8.UTF16LE|convert.iconv.UTF8.CSISO2022KR|convert.iconv.UCS2.UTF8|convert.iconv.8859_3.UCS2",
+        b"1": "convert.iconv.ISO88597.UTF16|convert.iconv.RK1048.UCS-4LE|convert.iconv.UTF32.CP1167|convert.iconv.CP9066.CSUCS4",
+        b"2": "convert.iconv.L5.UTF-32|convert.iconv.ISO88594.GB13000|convert.iconv.CP949.UTF32BE|convert.iconv.ISO_69372.CSIBM921",
+        b"3": "convert.iconv.L6.UNICODE|convert.iconv.CP1282.ISO-IR-90|convert.iconv.ISO6937.8859_4|convert.iconv.IBM868.UTF-16LE",
+        b"4": "convert.iconv.CP866.CSUNICODE|convert.iconv.CSISOLATIN5.ISO_6937-2|convert.iconv.CP950.UTF-16BE",
+        b"5": "convert.iconv.UTF8.UTF16LE|convert.iconv.UTF8.CSISO2022KR|convert.iconv.UTF16.EUCTW|convert.iconv.8859_3.UCS2",
+        b"6": "convert.iconv.INIS.UTF16|convert.iconv.CSIBM1133.IBM943|convert.iconv.CSIBM943.UCS4|convert.iconv.IBM866.UCS-2",
+        b"7": "convert.iconv.851.UTF-16|convert.iconv.L1.T.618BIT|convert.iconv.ISO-IR-103.850|convert.iconv.PT154.UCS4",
+        b"8": "convert.iconv.ISO2022KR.UTF16|convert.iconv.L6.UCS2",
+        b"9": "convert.iconv.CSIBM1161.UNICODE|convert.iconv.ISO-IR-156.JOHAB",
+        b"A": "convert.iconv.8859_3.UTF16|convert.iconv.863.SHIFT_JISX0213",
+        b"a": "convert.iconv.CP1046.UTF32|convert.iconv.L6.UCS-2|convert.iconv.UTF-16LE.T.61-8BIT|convert.iconv.865.UCS-4LE",
+        b"B": "convert.iconv.CP861.UTF-16|convert.iconv.L4.GB13000",
+        b"b": "convert.iconv.JS.UNICODE|convert.iconv.L4.UCS2|convert.iconv.UCS-2.OSF00030010|convert.iconv.CSIBM1008.UTF32BE",
+        b"C": "convert.iconv.UTF8.CSISO2022KR",
+        b"c": "convert.iconv.L4.UTF32|convert.iconv.CP1250.UCS-2",
+        b"D": "convert.iconv.INIS.UTF16|convert.iconv.CSIBM1133.IBM943|convert.iconv.IBM932.SHIFT_JISX0213",
+        b"d": "convert.iconv.INIS.UTF16|convert.iconv.CSIBM1133.IBM943|convert.iconv.GBK.BIG5",
+        b"E": "convert.iconv.IBM860.UTF16|convert.iconv.ISO-IR-143.ISO2022CNEXT",
+        b"e": "convert.iconv.JS.UNICODE|convert.iconv.L4.UCS2|convert.iconv.UTF16.EUC-JP-MS|convert.iconv.ISO-8859-1.ISO_6937",
+        b"F": "convert.iconv.L5.UTF-32|convert.iconv.ISO88594.GB13000|convert.iconv.CP950.SHIFT_JISX0213|convert.iconv.UHC.JOHAB",
+        b"f": "convert.iconv.CP367.UTF-16|convert.iconv.CSIBM901.SHIFT_JISX0213",
+        b"g": "convert.iconv.SE2.UTF-16|convert.iconv.CSIBM921.NAPLPS|convert.iconv.855.CP936|convert.iconv.IBM-932.UTF-8",
+        b"G": "convert.iconv.L6.UNICODE|convert.iconv.CP1282.ISO-IR-90",
+        b"H": "convert.iconv.CP1046.UTF16|convert.iconv.ISO6937.SHIFT_JISX0213",
+        b"h": "convert.iconv.CSGB2312.UTF-32|convert.iconv.IBM-1161.IBM932|convert.iconv.GB13000.UTF16BE|convert.iconv.864.UTF-32LE",
+        b"I": "convert.iconv.L5.UTF-32|convert.iconv.ISO88594.GB13000|convert.iconv.BIG5.SHIFT_JISX0213",
+        b"i": "convert.iconv.DEC.UTF-16|convert.iconv.ISO8859-9.ISO_6937-2|convert.iconv.UTF16.GB13000",
+        b"J": "convert.iconv.863.UNICODE|convert.iconv.ISIRI3342.UCS4",
+        b"j": "convert.iconv.CP861.UTF-16|convert.iconv.L4.GB13000|convert.iconv.BIG5.JOHAB|convert.iconv.CP950.UTF16",
+        b"K": "convert.iconv.863.UTF-16|convert.iconv.ISO6937.UTF16LE",
+        b"k": "convert.iconv.JS.UNICODE|convert.iconv.L4.UCS2",
+        b"L": "convert.iconv.IBM869.UTF16|convert.iconv.L3.CSISO90|convert.iconv.R9.ISO6937|convert.iconv.OSF00010100.UHC",
+        b"l": "convert.iconv.CP-AR.UTF16|convert.iconv.8859_4.BIG5HKSCS|convert.iconv.MSCP1361.UTF-32LE|convert.iconv.IBM932.UCS-2BE",
+        b"M": "convert.iconv.CP869.UTF-32|convert.iconv.MACUK.UCS4|convert.iconv.UTF16BE.866|convert.iconv.MACUKRAINIAN.WCHAR_T",
+        b"m": "convert.iconv.SE2.UTF-16|convert.iconv.CSIBM921.NAPLPS|convert.iconv.CP1163.CSA_T500|convert.iconv.UCS-2.MSCP949",
+        b"N": "convert.iconv.CP869.UTF-32|convert.iconv.MACUK.UCS4",
+        b"n": "convert.iconv.ISO88594.UTF16|convert.iconv.IBM5347.UCS4|convert.iconv.UTF32BE.MS936|convert.iconv.OSF00010004.T.61",
+        b"O": "convert.iconv.CSA_T500.UTF-32|convert.iconv.CP857.ISO-2022-JP-3|convert.iconv.ISO2022JP2.CP775",
+        b"o": "convert.iconv.JS.UNICODE|convert.iconv.L4.UCS2|convert.iconv.UCS-4LE.OSF05010001|convert.iconv.IBM912.UTF-16LE",
+        b"P": "convert.iconv.SE2.UTF-16|convert.iconv.CSIBM1161.IBM-932|convert.iconv.MS932.MS936|convert.iconv.BIG5.JOHAB",
+        b"p": "convert.iconv.IBM891.CSUNICODE|convert.iconv.ISO8859-14.ISO6937|convert.iconv.BIG-FIVE.UCS-4",
+        b"q": "convert.iconv.SE2.UTF-16|convert.iconv.CSIBM1161.IBM-932|convert.iconv.GBK.CP932|convert.iconv.BIG5.UCS2",
+        b"Q": "convert.iconv.L6.UNICODE|convert.iconv.CP1282.ISO-IR-90|convert.iconv.CSA_T500-1983.UCS-2BE|convert.iconv.MIK.UCS2",
+        b"R": "convert.iconv.PT.UTF32|convert.iconv.KOI8-U.IBM-932|convert.iconv.SJIS.EUCJP-WIN|convert.iconv.L10.UCS4",
+        b"r": "convert.iconv.IBM869.UTF16|convert.iconv.L3.CSISO90|convert.iconv.ISO-IR-99.UCS-2BE|convert.iconv.L4.OSF00010101",
+        b"S": "convert.iconv.INIS.UTF16|convert.iconv.CSIBM1133.IBM943|convert.iconv.GBK.SJIS",
+        b"s": "convert.iconv.IBM869.UTF16|convert.iconv.L3.CSISO90",
+        b"T": "convert.iconv.L6.UNICODE|convert.iconv.CP1282.ISO-IR-90|convert.iconv.CSA_T500.L4|convert.iconv.ISO_8859-2.ISO-IR-103",
+        b"t": "convert.iconv.864.UTF32|convert.iconv.IBM912.NAPLPS",
+        b"U": "convert.iconv.INIS.UTF16|convert.iconv.CSIBM1133.IBM943",
+        b"u": "convert.iconv.CP1162.UTF32|convert.iconv.L4.T.61",
+        b"V": "convert.iconv.CP861.UTF-16|convert.iconv.L4.GB13000|convert.iconv.BIG5.JOHAB",
+        b"v": "convert.iconv.UTF8.UTF16LE|convert.iconv.UTF8.CSISO2022KR|convert.iconv.UTF16.EUCTW|convert.iconv.ISO-8859-14.UCS2",
+        b"W": "convert.iconv.SE2.UTF-16|convert.iconv.CSIBM1161.IBM-932|convert.iconv.MS932.MS936",
+        b"w": "convert.iconv.MAC.UTF16|convert.iconv.L8.UTF16BE",
+        b"X": "convert.iconv.PT.UTF32|convert.iconv.KOI8-U.IBM-932",
+        b"x": "convert.iconv.CP-AR.UTF16|convert.iconv.8859_4.BIG5HKSCS",
+        b"Y": "convert.iconv.CP367.UTF-16|convert.iconv.CSIBM901.SHIFT_JISX0213|convert.iconv.UHC.CP1361",
+        b"y": "convert.iconv.851.UTF-16|convert.iconv.L1.T.618BIT",
+        b"Z": "convert.iconv.SE2.UTF-16|convert.iconv.CSIBM1161.IBM-932|convert.iconv.BIG5HKSCS.UTF16",
+        b"z": "convert.iconv.865.UTF16|convert.iconv.CP901.ISO6937",
+        b"/": "convert.iconv.IBM869.UTF16|convert.iconv.L3.CSISO90|convert.iconv.UCS2.UTF-8|convert.iconv.CSISOLATIN6.UCS-4",
+        b"+": "convert.iconv.UTF8.UTF16|convert.iconv.WINDOWS-1258.UTF32LE|convert.iconv.ISIRI3342.ISO-IR-157",
     }
 
 
